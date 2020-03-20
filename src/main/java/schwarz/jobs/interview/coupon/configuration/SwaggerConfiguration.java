0e46f37bee40ba6d21cd61ee83e8e7cf7b76d65a
Kevin Krauskopf <Kevin.Krauskopf@lidl.com>
2020-03-20 15:34:46 +0100

    exclude random swagger stuff

--- src/main/java/schwarz/jobs/interview/coupon/configuration/SwaggerConfiguration.java
+++ src/main/java/schwarz/jobs/interview/coupon/configuration/SwaggerConfiguration.java
@@ -18,7 +18,7 @@ public class SwaggerConfiguration {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
             .select()
-            .apis(RequestHandlerSelectors.any())
+            .apis(RequestHandlerSelectors.basePackage("schwarz.jobs.interview"))
             .paths(PathSelectors.any())
             .build();
     }
