34f6e93514a04efb9d8c921cb75bd45dd08585e9
Kevin Krauskopf <Kevin.Krauskopf@lidl.com>
2020-03-20 15:00:30 +0100

    h2-setup

---
+++ src/main/resources/data.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS billionaires;
+ 
+CREATE TABLE coupons (
+  id INT AUTO_INCREMENT  PRIMARY KEY,
+  code  VARCHAR(250) NOT NULL,
+  discount VARCHAR(250) NOT NULL,
+  minBasketValue VARCHAR(250) DEFAULT NULL
+);
+ 
+INSERT INTO coupons (code, discount, minBasketValue) VALUES
+  ('TEST1', 10.00, 50.00),
+  ('TEST2', 15.00, 100.00),
+  ('TEST3', 20.00, 200.00);
