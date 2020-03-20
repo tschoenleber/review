6eacd334edd31aa04a568de81238ed853f09aaf3
Robert Blesch <robert.blesch@mail.schwarz>
2020-03-20 15:10:02 +0100

    Implemented simple business logic

--- src/main/java/schwarz/jobs/interview/coupon/core/domain/Coupon.java
+++ src/main/java/schwarz/jobs/interview/coupon/core/domain/Coupon.java
@@ -1,11 +1,31 @@
 package schwarz.jobs.interview.coupon.core.domain;
 
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 @Entity
+@Data
+@Builder
+@NoArgsConstructor
 public class Coupon {
 
     @Id
     private Long id;
+
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "discount", precision = 10, scale = 2)
+    private BigDecimal discount;
+
+    @Column(name = "mindBaketValue", precision = 10, scale = 2)
+    private BigDecimal minBasketValue;
+
 }
